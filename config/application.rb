af9e1bd8ccd2998b2cd13bda99d3007cd9912295
Nick-Damico <nickalan82@icloud.com>
2018-11-23 21:59:15 -0500

    Add migration for Users, Add has_sercure_password macro on User model

--- config/application.rb
+++ config/application.rb
@@ -5,9 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module RailsAmusementPark
   class Application < Rails::Application
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
